fa5f35eb27b99c3f9d99d49247c8c529afcc34f4
Pavel Kalian <pavel@kalian.cz>
2021-05-27 19:45:09 -0300

    Add script for automatic generation of IENC catalogs from Danubeportal

---
+++ danube_ienc.py
@@ -0,0 +1,95 @@
+#!/usr/bin/python
+"""Script to process the HTML Danube IENC charts list and convert it to the XML catalog format
+Part of the ChartCatalogs project
+Copyright (c) 2021 Pavel Kalian
+Licensed under GPLv2 or, at yoir will later version
+"""
+
+
+import sys
+from HTMLParser import HTMLParser
+from ChartCatalogs import Chart, RncChartCatalog
+from datetime import datetime
+import codecs
+from pprint import pprint
+
+class DanubeIENCHTMLParser(HTMLParser):
+    def __init__(self):
+        HTMLParser.__init__(self)
+        self.inColumn = False
+        self.chart = None
+        self.column = 0
+        self.row = 0
+        self.base_url = ''
+        self.catalog = RncChartCatalog()
+        self.catalog.title = "Danube IENC Charts"
+        self.inLink = False
+
+    def handle_starttag(self, tag, attrs):
+        if tag == 'tr':
+            self.chart = Chart()
+            self.chart.chart_format = 'Sailing Chart, International Chart'
+        elif tag == 'td':
+            self.inColumn = True
+            self.column += 1
+        elif tag == 'a' and self.inColumn and self.column == 7:
+            for attr in attrs:
+                if attr[0] == 'href':
+                    self.chart.url = self.base_url + attr[1]
+        elif tag == 'a' and self.inColumn and self.column == 2:
+              for attr in attrs:
+                  if attr[0] == 'href':
+                      self.inLink = True;
+
+    def handle_endtag(self, tag):
+        if tag == 'tr':
+            self.chart.number = "Base" + str(self.row + 1)
+            if self.row > 0 and self.chart.is_valid():
+                self.catalog.add_chart(self.chart)
+            else:
+                if self.row > 0:
+                    print "<!-- unavailable/invalid?"
+                    pprint(vars(self.chart))
+                    print "-->"
+            self.column = 0
+            self.row += 1
+        elif tag == 'td':
+            self.inColumn = False
+
+    def handle_data(self, data):
+        if self.inColumn:
+            if self.inColumn:
+                if self.column == 2:
+                    self.chart.title = data.strip()
+                if self.column == 3:
+                    self.chart.title += " " + data.strip()
+                if self.column == 7:
+                    if self.inLink and data.strip() != '':
+                        self.chart.title = data.strip()
+                        self.inLink = False
+                if self.column == 6:
+                    try:
+                        self.chart.zipfile_ts = datetime.strptime(data.strip(), '%Y-%m-%d')
+                    except:
+                        pass
+                if self.column == 5:
+                    self.chart.ntm_edition_last_correction = "0"
+
+    def print_xml(self):
+        self.catalog.print_xml(True)
+
+if len(sys.argv) < 2:
+    print 'ERROR: Filename parameter missing'
+if len(sys.argv) < 3:
+    print 'ERROR: Base URL parameter is missing'
+    print 'Usage danube_ienc.py <filename> <title>'
+    exit(1)
+
+
+# instantiate the parser and fed it some HTML
+f = codecs.open(sys.argv[1], 'r', 'utf-8')
+parser = DanubeIENCHTMLParser()
+#parser.base_url = sys.argv[2]
+parser.catalog.title = sys.argv[2]
+parser.feed(f.read())
+parser.print_xml()
---
+++ danube_ienc.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+# Script to download Brasilian RNC charts list and convert it to the XML catalog format 
+#
+# Part of the ChartCatalogs project
+# Copyright (c) 2021 Pavel Kalian
+# Licensed under GPLv2 or, at yoir will later version
+#
+
+#set -x
+
+if [ -z "${CHARTCATALOGS_TARGETDIR}" ]; then
+  TARGETDIR='.'
+else
+  TARGETDIR="${CHARTCATALOGS_TARGETDIR}"
+fi
+
+CATALOGNAME='BR_RNC_Catalog.xml'
+
+function process_country() {
+  CATALOGNAME="$1"
+  URL="$2"
+  TITLE="$3"
+
+  WORKDIR='/tmp'
+  if curl -f -o "${WORKDIR}/danube_ienc.html" "$URL"; then
+    ./danube_ienc.py ${WORKDIR}/danube_ienc.html "" > ${WORKDIR}/${CATALOGNAME}
+    if xmllint --noout ${WORKDIR}/${CATALOGNAME} 2>&1 >/dev/null; then
+      cat ${WORKDIR}/${CATALOGNAME} | xmllint --format - >  ${TARGETDIR}/${CATALOGNAME}
+      rm ${WORKDIR}/${CATALOGNAME}
+    fi
+  fi
+
+  if [ -f ${WORKDIR}/danube_ienc.html ]; then
+    rm ${WORKDIR}/danube_ienc.html
+  fi
+}
+
+process_country "AT_IENC_Catalog.xml" "https://www.danubeportal.com/charts/electronicChart/AT" "Austria IENC Charts"
+process_country "SK_IENC_Catalog.xml" "https://www.danubeportal.com/charts/electronicChart/SK" "Slovakia IENC Charts"
+process_country "HU_IENC_Catalog.xml" "https://www.danubeportal.com/charts/electronicChart/HU" "Hungary IENC Charts"
+process_country "HR_IENC_Catalog.xml" "https://www.danubeportal.com/charts/electronicChart/HR" "Croatia IENC Charts"
+process_country "RS_IENC_Catalog.xml" "https://www.danubeportal.com/charts/electronicChart/RS" "Serbia IENC Charts"
+process_country "BG_IENC_Catalog.xml" "https://www.danubeportal.com/charts/electronicChart/BG" "Bulgaria IENC Charts"
+process_country "RO_IENC_Catalog.xml" "https://www.danubeportal.com/charts/electronicChart/RO" "Romania IENC Charts"
