5891febf29bbae06877859996dbed559f0347aee
Pavel Kalian <nohal@users.noreply.github.com>
2023-03-29 13:19:44 -0300

    Make the NL updater use upstream filenames
    Implement https://github.com/chartcatalogs/scripts/pull/5 in python2 compatible way

--- nl_ienc.py
+++ nl_ienc.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 """Script to process the JSON feed of the Dutch IENC charts list and convert it to the XML catalog format
 Part of the ChartCatalogs project
-Copyright (c) 2019-2020 Marcel Verpaalen
+Copyright (c) 2019-2023 Marcel Verpaalen
 Licensed under GPLv2 or, at your will later version
 """
 
@@ -9,13 +9,15 @@ import sys
 from ChartCatalogs import Chart, RncChartCatalog
 from datetime import datetime
 import json
+from urllib import urlopen
+import re
 
 catalog = RncChartCatalog()
 catalog.title = "Netherlands Inland ENC Charts"
 
 with open(sys.argv[1]) as f:
     data = json.load(f)
-    cnt=0
+    cnt = 0
     for tileset in data:
         chart = Chart()
         chart.chart_format = 'Sailing Chart, International Chart'
@@ -24,8 +26,15 @@ with open(sys.argv[1]) as f:
         chart.title = "%s" % tileset['name']
         chart.zipfile_ts = datetime.fromtimestamp(tileset['date']/1000)
         chart.target_filename = "%s.zip" % tileset['name']
+        try:
+            response = urlopen(chart.url)
+            d = response.headers['content-disposition']
+            filename = re.findall("filename=(.+)", d)[0]
+            if filename is not None:
+                chart.target_filename = filename
+        except Exception:
+            pass
         catalog.add_chart(chart)
-        cnt=cnt+1
+        cnt = cnt + 1
 
 catalog.print_xml(True)
-
