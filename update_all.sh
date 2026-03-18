1b171d89a353f32c284bfa1d350e231caa1facab
Pavel Kalian <pavel@kalian.cz>
2026-03-18 15:16:27 -0300

    Fix and reenable the DE catalog

--- update_all.sh
+++ update_all.sh
@@ -1,7 +1,7 @@
 cd "$(dirname $0)"
 #Must be updated manually as there now is cloudflare captcha in fron tof the site ./brasil_rnc.sh
 #Must be updated manually as there now is cloudflare captcha in fron tof the site ./brasil_ienc.py > BR_IENC_Catalog.xml || rm BR_IENC_Catalog.xml
-#./de_ienc.sh
+./de_ienc.sh
 python3 euris_inc.py
 #./noaa_mbtiles.sh
 # Moved to hourly (update_nl.sh) - ./nl_ienc.sh
